66ff7426fdceebf187c5778192c82b363b062118
TerminatorxxD <tuanlinhdoan2@gmail.com>
2023-09-04 16:00:51 +0700

    fix/env-leak

--- component/deploy-button/deploy-button.tsx
+++ component/deploy-button/deploy-button.tsx
@@ -17,67 +17,67 @@ export const DeployButton = () => {
                             key: "NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY",
                             target: "production",
                             type: "encrypted",
-                            value: "pk_test_ZGVlcC1idWxsZG9nLTk0LmNsZXJrLmFjY291bnRzLmRldiQ"
+                            value: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
                         },
                         {
                             key: "CLERK_SECRET_KEY",
                             target: "production",
                             type: "encrypted",
-                            value: "sk_test_Fe84KIx7cuQIj1DAvr0YGnAhQ6WfjXjk7WW3nSi6Vp"
+                            value: process.env.CLERK_SECRET_KEY
                         },
                         {
                             key: "NEXT_PUBLIC_CLERK_SIGN_IN_URL",
                             target: "production",
                             type: "encrypted",
-                            value: "/sign-in"
+                            value: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL
                         },
                         {
                             key: "NEXT_PUBLIC_CLERK_SIGN_UP_URL",
                             target: "production",
                             type: "encrypted",
-                            value: "/sign-up"
+                            value: process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL
                         },
                         {
                             key: "NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL",
                             target: "production",
                             type: "encrypted",
-                            value: "/"
+                            value: process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL
                         },
                         {
                             key: "NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL",
                             target: "production",
                             type: "encrypted",
-                            value: "/"
+                            value: process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL
                         },
                         {
                             key: "NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME",
                             target: "production",
                             type: "encrypted",
-                            value: "greenwich-university"
+                            value: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME
                         },
                         {
                             key: "DATABASE_URL",
                             target: "production",
                             type: "encrypted",
-                            value: "mysql://wjram64zq1m69hg6h11z:pscale_pw_kzMJBWTdhXtiuTNQZKgliBDJyczWGyJ6OtLJ8CnHgSU@aws.connect.psdb.cloud/ecommerce-admin?sslaccept=strict"
+                            value: process.env.DATABASE_URL
                         },
                         {
                             key: "STRIPE_API_KEY",
                             target: "production",
                             type: "encrypted",
-                            value: "sk_test_51Nl79iGCDWPN2bUUosHQ0tFfELvEfoCl2tDik27OvZQd8kgX0NPryrGcb6YWt4bPVjbSexvIELnBHuAGNYok3XmT00HaHZEsvM"
+                            value: process.env.STRIPE_API_KEY
                         },
                         {
                             key: "FRONTEND_STORE_URL",
                             target: "production",
                             type: "encrypted",
-                            value: "http://localhost:3001"
+                            value: process.env.FRONTEND_STORE_URL
                         },
                         {
                             key: "STRIPE_WEBHOOK_SECRET",
                             target: "production",
                             type: "encrypted",
-                            value: "whsec_d8e43e430972bdc57e9eaf781897bbacd5d22877de00dc83f64909be5ac048a3"
+                            value: process.env.STRIPE_WEBHOOK_SECRET
                         }
                     ],
                     gitRepository: {
